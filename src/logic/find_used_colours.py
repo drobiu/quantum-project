5eb5d31b3d22c8048245c0c6352fe1bfdca53905
Tiamo van Eijmeren <t.vaneijmeren@student.tudelft.nl>
2022-01-19 15:48:36 +0100

    Continue with FUC algorithm

--- src/logic/find_used_colours.py
+++ src/logic/find_used_colours.py
@@ -1,8 +1,9 @@
-from qiskit import QuantumRegister, QuantumCircuit
+from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
 from src.logic.oracles import oracle_b
 from src.logic.query import query
 from src.arithmetic.counter import mincount
 from src.arithmetic.comparator import comparator
+from src.util.util import run_qc
 
 
 def find_used_colours(n_positions, n_colors, secret_string):
@@ -18,8 +19,9 @@ def find_used_colours(n_positions, n_colors, secret_string):
     f = QuantumRegister(1)
     g = QuantumRegister(1)
     h = QuantumRegister(1)
+    cl = ClassicalRegister(4)
 
-    qc = QuantumCircuit(a, b, c, d, e, f, g, h)
+    qc = QuantumCircuit(a, b, c, d, e, f, g, h, cl)
 
     # Step 1
     qc = oracle_b(qc, c, a, s)
@@ -34,7 +36,7 @@ def find_used_colours(n_positions, n_colors, secret_string):
     qc.barrier()
 
     # Step 4
-    qc = oracle_b(qc, c, d)
+    qc = oracle_b(qc, c, d, s)
     qc.barrier()
 
     # Step 5
@@ -55,10 +57,10 @@ def find_used_colours(n_positions, n_colors, secret_string):
 
     # Step 8
     qc.cx(d[0], h)
-    qc.cx([e[0]].append(f).append(g), h)
+    qc.cx([e[0], f[0], g[0]], h)
     qc.x(f)
-    qc.cx([a[0]].append(f).append(g), h)
-    qc.cx([b[0]].append(f).append(g), h)
+    qc.cx([e[0], f[0], g[0]], h)
+    qc.cx([b[0], f[0], g[0]], h)
     qc.x(f)
     qc.barrier()
 
@@ -66,7 +68,21 @@ def find_used_colours(n_positions, n_colors, secret_string):
     qc.z(h)
     qc.barrier()
 
-    # TODO: Step 10, 11, 12
+    # TODO: Step 10
+
+    #  Step 11
+    qc.h(b[:])
+    qc.barrier()
+
+    #  Step 12
+    qc.measure(b[:], cl[:])
 
     return qc
+
+
+if __name__ == "__main__":
+    qc = find_used_colours(4, 4, [0, 0, 0, 0])
+
+    run_qc(qc, with_QI=False)
+    qc.draw(output="text")
 
