f65251cc3dcd88d47c9b7dce6c81d7eced48bd8b
pietervzwe <pcjavz@gmail.com>
2022-01-19 15:31:35 +0100

    final version

--- src/arithmetic/add.py
+++ src/arithmetic/add.py
@@ -1,28 +1,33 @@
 from math import pi
 from qiskit import *
 from qiskit.circuit.library.standard_gates import PhaseGate
-from mastermind.arithmetic.qft import qft, iqft
+from qiskit.circuit.library.basis_change import QFT
 
-def add(circuit,a,b,apply_QFT=true,amount=1):
+def add(circuit,a,b,apply_QFT=True,amount=1):
     #Function adds a to b
     #Original add function build by https://github.com/TimVroomans/Quantum-Mastermind/blob/master/src/mastermind/arithmetic/dradder.py
     number_a=len(a)
     number_b=len(b)
+    num=number_b+number_a
+
 
     if number_a > number_b:
         raise ValueError("Amount of registers in b must be larger than a")
 
     # QFT(optional)
     if apply_QFT:
-            qft(circuit, b)
+        circuit = circuit.compose(QFT(num_qubits=num, approximation_degree=0, do_swaps=True,
+                            inverse=False, insert_barriers=True, name='qft'))
 
     # Actual add loop
     for i in range(number_a):
         for j in range(number_b - i):
-            circuit.cp(amount * pi / 2 ** (nb - i - j - 1), a[i], b[j])
+            circuit.cp(amount * pi / 2 ** (number_b - i - j - 1), a[i], b[j])
 
     #Inverse QFT (optional)
     if apply_QFT:
-        iqft(circuit, b)
+        circuit = circuit.compose(QFT(num_qubits=num, approximation_degree=0, do_swaps=True,
+                            inverse=True, insert_barriers=True, name='iqft'))
 
     return circuit
+
